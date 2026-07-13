da1f3273b52473f13f601caa7c7b3423c2552270
Shahid Anwaar <shahidalirajpoot2@gmail.com>
2026-07-13 16:49:54 +0500

    Added origin in cors of new frontend

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://tailor-master.netlify.app",
+  "https://tailor-flow.netlify.app"
 ].filter(Boolean);
 
 const app = express();
